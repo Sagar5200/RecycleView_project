410fbd3995fa2c79061501bf24d1f3d9483b418d
Sagarkumar Baravaliya <62414254+Sagar5200@users.noreply.github.com>
2022-06-22 21:29:38 +1200

    stage one completed

--- app/src/main/java/com/example/recycleview_project/MainActivity.kt
+++ app/src/main/java/com/example/recycleview_project/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
